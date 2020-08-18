080bb099113111724b645331ed6945d2a73b94f7
cchromak <cchromak@gmail.com>
2020-08-18 18:35:52 -0400

    added sign in

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 var description = document.getElementById("description");
 
-function moveLeft() {
-    var left = parseInt(window.getComputedStyle(description).getPropertyValue("left"));
-    if (left > 0) {
-        description.style.left = left - 2 + "px";
-    }
-       
-}
 
-function display() {
-    document.getElementById("singIn").innerHTML="";
-}
